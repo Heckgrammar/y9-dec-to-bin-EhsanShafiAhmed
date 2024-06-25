29b557fad5bfa727d554444158084013f914f7c3
Ehsan Shafi - Ahmed <21EShafi-Ahmed@heckgrammar.co.uk>
2024-06-25 15:15:38 +0100

    started main task

--- Program.cs
+++ Program.cs
@@ -13,20 +13,26 @@ namespace Y9_DEC_TO_BIN_SKELETON
             //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
             int myInt = 0; //just for testing single stepping
             string myString = "12"; //watch me being cast from string to int
+
+            //TASK 1:
             int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
 
-            //MAIN:  NUMBER CONVERSION PROGRAM
+          
+            //TASK 2:
 
-            Console.WriteLine(numberConversion(210, 2));
+            int insertmyString = Convert.ToInt32(myString.Insert(0,"Your number is "));
+
+            //MAIN:  NUMBER CONVERSION PROGRAM
+            Console.WriteLine("Enter your number");
+            Console.WriteLine(numberConversion(Convert.ToInt32(Console.ReadLine()), 2));
         }
 
         //static void means the function will not return a value so it does not need a data type 
         //...this function DOES return a value so the method must have a data type
         static string numberConversion(int number, int numberbase)
         {
-            Console.WriteLine("Enter your number");
-            number = Console.Read();
-            return Convert.ToString(number%numberbase); ;
+            string binaryArray = { };
+            return Convert.ToString(number % numberbase);
         }
     }
 }
